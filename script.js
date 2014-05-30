9832f9f184685e88e7f97e5f32dbcbc86d27f1b4
NWChen <neilwchen@gmail.com>
2014-05-30 18:26:08 -0400

    Added user input for time limits

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-var work = 6;
-var relax = 3;
 var time = 0;
 var working = true;
 var isPaused = true;
 
 function startTime(){
 	var t = window.setInterval(function(){
+		var work = document.getElementById("worktime").value;
+		console.log(work);
+		var relax = document.getElementById("relaxtime").value;
 		if(working && !isPaused){
-			if(time < work) incrementTime();
+			if(time <= work) incrementTime();
 			else{ time = 0; working = false; }
 		}
 		else if(!isPaused){
-			if(time < relax) incrementTime();
+			if(time <= relax) incrementTime();
 			else{ time = 0; working = true; }
 		}
 	}, 1000);
 }
 
 function incrementTime(){
-	time++;
 	document.getElementById("time").innerHTML = checkTime(time/60)+":"+checkTime(time%60);
+	time++;
 }
 
 function checkTime(i){
@@ -33,4 +34,8 @@ function changeButton(){
 	element = document.getElementById("button");
 	if(element.innerHTML==="Start") element.innerHTML = "Stop";
 	else element.innerHTML = "Start";
+}
+
+function changeLimits(){
+	
 }
