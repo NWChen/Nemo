0d7bc9e573a600a0ada0520986c843e24fc2ad2b
NWChen <neilwchen@gmail.com>
2014-05-30 23:54:56 -0400

    Added notification for time is up

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-var time = 0;
+var time = 60;
 var working = true;
 var isPaused = true;
 
 function startTime(){
+	var sound = new Audio("sound.wav");
 	var t = window.setInterval(function(){
 		var work = (60*ascertainZero(document.getElementById("workminutes").value)) + ascertainZero(document.getElementById("workseconds").value);
 		console.log(work);
 		var relax = (60*ascertainZero(document.getElementById("relaxminutes").value)) + ascertainZero(document.getElementById("relaxseconds").value);
 		if(working && !isPaused){
-			if(time <= work) incrementTime();
-			else{ time = 0; working = false; }
+			if(time <= work){ incrementTime(); document.getElementById("time").style.color = "#f5f5f5"; }
+			else{ sound.play(); time = 0; working = false; document.getElementById("time").style.color = "#c0392b"; document.getElementById("status").innerHTML = "relaxing"; }
 		}
 		else if(!isPaused){
-			if(time <= relax) incrementTime();
-			else{ time = 0; working = true; }
+			if(time <= relax){ incrementTime(); document.getElementById("time").style.color = "#f5f5f5"; }
+			else{ sound.play(); time = 0; working = true; document.getElementById("time").style.color = "#c0392b"; document.getElementById("status").innerHTML = "working"; }
 		}
 	}, 1000);
 }
